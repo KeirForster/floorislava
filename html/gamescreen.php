76cbbf1d3801c704b3d906ea07902ae5f355b1c9
Keir Forster <keirforster@gmail.com>
2018-02-09 02:14:26 -0800

    updated homepage layout and passed high score values to game page

--- html/gamescreen.php
+++ html/gamescreen.php
@@ -1,29 +1,46 @@
-<html>
+<?php
+    // Start the session
+    session_start();
+?>
+
+<html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
     <title>FloorIsLava</title>
-    <!--  Bootstrap  -->
+
+    <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 </head>
 
 <body class="text-center">
-    <h1>Gamescreen</h1>
-    <?php
-        if (isset($_POST['username']))
-        {
-            echo '<h3>Welcome: ' . $_POST['username'] . '</h3>';
-        }
-        
-        else
-        {
-            echo 'no username set!';
-        }
-    ?>
+<h1>Gamescreen</h1>
+<?php
+    if (isset($_POST['username']))
+    {
+        echo '<h3>Welcome: ' . $_POST['username'] . '</h3>';
+        echo '<h3>Highest score:</h3>';
+        echo "<h3>" . $_SESSION['highscoreUsername'] . "</h3>";
+        echo "<h3>" . $_SESSION['highscore'] . "</h3>";
+    }
+    
+    else
+    {
+        echo 'no username set!';
+    }
+?>
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
         crossorigin="anonymous"></script>
