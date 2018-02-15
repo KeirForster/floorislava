6e2b80f83fc0e82afd450dcf7290d131199b0a60
Hawk\garel <garelbucknor@gmail.com>
2018-02-14 18:17:16 -0800

    Added volcano.png to the img directory. Made it the favicon for index and gamescreen.

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="icon" href="img/volcano.png">
     <title>Dodge the Lava</title>
 
     <!-- Bootstrap core CSS -->
